7a8398761d8cf3e27ec4820b38a801c7ceb97429
nico <nicolaspucci1989@gmail.com>
2019-11-13 14:16:34 -0300

    agregando vue para productos

---
+++ js/productos.js
@@ -0,0 +1,68 @@
+const productos= [
+    {
+        name: 'Proteina Animal',
+        subProductos: ['Carne de Vaca', 'Cerdo', 'Pollo', 'Pescado'],
+        imagen: 'img/productos/proteina-animal.jpg',
+        color: 'red'
+    },
+    {
+        name: 'Granos',
+        subProductos: ['Haba de Soja', 'Maíz', 'Trigo', 'Sorgo', 'Palomitas de Maíz'],
+        imagen: 'img/productos/granos.jpg',
+        color: 'amber lighten-1'
+    },
+    {
+        name: 'Harinas',
+        subProductos: ['Soja', 'Trigo', 'Maíz'],
+        imagen: 'img/productos/harina.jpg',
+        color: 'light-green darken-2'
+    },
+    {
+        name: 'Semillas y Aceites',
+        subProductos: ['Girasol', 'Sésamo', 'Linaza'],
+        imagen: 'img/productos/semillas-aceites.jpg',
+        color: 'light-green lighten-1'
+    },
+    {
+        name: 'Productos Lácteos',
+        subProductos: ['Quesos', 'Manteca'],
+        imagen: 'img/productos/productos-lacteos.jpg',
+        color: 'cyan lighten-2'
+    },
+    {
+        name: 'Bebidas',
+        subProductos: ['Vino Tinto', 'Vino Blanco', 'Cerveza', 'Agua Mineral'],
+        imagen: 'img/productos/vinos.jpg',
+        color: 'deep-purple lighten-1'
+    },
+    {
+        name: '',
+        subProductos: [],
+        imagen: 'img/productos/.jpg',
+        color: ''
+    },
+    {
+        name: '',
+        subProductos: [],
+        imagen: 'img/productos/.jpg',
+        color: ''
+    },
+    {
+        name: '',
+        subProductos: [],
+        imagen: 'img/productos/.jpg',
+        color: ''
+    },
+    {
+        name: '',
+        subProductos: [],
+        imagen: 'img/productos/.jpg',
+        color: ''
+    },
+    {
+        name: '',
+        subProductos: [],
+        imagen: 'img/productos/.jpg',
+        color: ''
+    },
+];
